85a2752b9095a7e7745e8b2046c2bf27d844b1df
zhangshubo <zhangshubo1992@gmail.com>
2018-01-28 13:59:45 +0800

    test

--- test.cpp
+++ test.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main()
 {
     std::cout << "test" << std::endl;
+    string s = "123";
+    int i = int(s[0]);
 
+    cout << i << endl;
     return 0;
 }
