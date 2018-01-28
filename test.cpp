97986f684418b04975cc79fc0bf27cf6dfef49bf
zhangshubo <zhangshubo1992@gmail.com>
2018-01-28 15:59:24 +0800

    test

--- test.cpp
+++ test.cpp
@@ -6,9 +6,18 @@ using namespace std;
 int main()
 {
     std::cout << "test" << std::endl;
+
+    //test string braket
     string s = "123";
     int i = int(s[0]);
 
     cout << i << endl;
+
+    //test nullptr
+
+    int *p = NULL;
+
+    cout << (p? 0:1) << endl;
+
     return 0;
 }
